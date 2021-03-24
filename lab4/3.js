48aee2bde07bcb287f98d512e4d151f108f15766
Сергей <maxim.2002.k@mail.ru>
2021-03-24 19:28:33 +0300

    Выполнил 4 лабу

--- lab4/3.js
+++ lab4/3.js
@@ -7,8 +7,34 @@
  * rle('BCCADDEEEBB') === 'BC2AD2E3B2'
  */
 
-function rle(str) {
-    //code here
+function rle(str)
+{
+    if(str.length < 2)
+    {
+        return str;
+    }
+    let k = 1;
+    let result = str[0];
+    for(let i = 1; i < str.length; i++)
+    {
+        if(str[i] === str[i - 1])
+        {
+            k++;
+        }
+        else
+            {
+            if(k > 1)
+            {
+                result = result.concat(String(k));
+            }
+            result = result.concat(str[i]);
+          k = 1;
+        }
+    }
+    if(k > 1)
+    {
+        return result.concat(String(k));
+    }
+    return result;
 }
-
 module.exports = rle;
