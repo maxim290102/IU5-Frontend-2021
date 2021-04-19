0837e36d76c8898355d4f8c39151ffaa37e2e24a
Сергей <maxim.2002.k@mail.ru>
2021-04-19 22:05:48 +0300

    исправил 4 лабу

--- lab4/1.js
+++ lab4/1.js
@@ -5,8 +5,7 @@
  * Примеры:
  * ['мир', 'Рим', 'сирота', 'Ариост', 'мри', 'пва', 'лор', 'авп']; -> [["мир", "Рим", "мри"], ["сирота", "Ариост"], ["пва", "авп"]]
  */
-function getAnagramms(arr)
-{
+function getAnagramms(arr) {
     let manArr = new Map();
     if (!arr || arr.length <= 1) {
         return arr;
@@ -14,7 +13,7 @@ function getAnagramms(arr)
     let firstElemSort = arr[0].toLowerCase().split('').sort().join('');
     manArr[firstElemSort] = [];
     manArr[firstElemSort].push(arr[0]);
-    for (let i = 1; i < arr.length; i++)
+    for (let i = 1; i < arr.length; i++) {
         let elemSort = arr[i].toLowerCase().split('').sort().join('');
         if (typeof manArr[elemSort] === 'object') {
             manArr[elemSort].push(arr[i]);
